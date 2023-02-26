c4ba5cc0c5dac73e71ad2ecc77d57e4b5c32d91e
PilipenkoKirill <www-kirill.pilipenko@yandex.ru>
2023-02-26 20:42:23 +0300

    Доработка нескольких работ

--- NormDistGen.py
+++ NormDistGen.py
@@ -2,7 +2,7 @@
 import numpy
 from random import randint
 mu, sigma = randint(1,12), randint(1,5)
-samples = numpy.random.normal(mu, sigma, 1000)
+samples = numpy.random.normal(mu, sigma, 20)
 file = open("normSamples.csv", "w")
 for s in samples:
     file.write(str(s).replace(".",",")+"\n")
